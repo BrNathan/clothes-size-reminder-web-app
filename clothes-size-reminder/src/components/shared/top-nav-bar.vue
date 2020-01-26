ec8253e336681b74a7b78ca4647d0dc58aa3eeaf
Nathan BRUET <nathan.bruet@gmail.com>
2020-01-26 16:10:25 +0100

    delete right btn of topnav bar

--- clothes-size-reminder/src/components/shared/top-nav-bar.vue
+++ clothes-size-reminder/src/components/shared/top-nav-bar.vue
@@ -22,13 +22,12 @@
       </div>
     </router-link>
 
-
     <v-spacer></v-spacer>
 
-    <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+    <!-- <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
       <span class="mr-2">Latest Release</span>
       <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
+    </v-btn> -->
   </v-app-bar>
 </template>
 
