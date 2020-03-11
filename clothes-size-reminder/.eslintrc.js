5c0b5e2f9456dbc3cbf934467c779e837a62011b
Nathan BRUET <nathan.bruet@gmail.com>
2020-03-11 23:08:59 +0100

    apply lint

--- clothes-size-reminder/.eslintrc.js
+++ clothes-size-reminder/.eslintrc.js
@@ -9,13 +9,13 @@ module.exports = {
     '@vue/typescript',
   ],
   plugins: [
-    //'unused-imports', // ONLY FOR CLEANING
+    // 'unused-imports', // ONLY FOR CLEANING
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-param-reassign': ['error', { props: false }],
-    //'unused-imports/no-unused-imports-ts': 2, // ONLY FOR CLEANING
+    // 'unused-imports/no-unused-imports-ts': 2, // ONLY FOR CLEANING
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
